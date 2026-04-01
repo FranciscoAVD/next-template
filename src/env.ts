13d8f712506e43043e298c25bb00ee4dc9408c47
Victoriano <fvictoriano@hrplabs.com>
2026-04-01 12:16:09 -0400

    [config] added resend as a dep

--- src/env.ts
+++ src/env.ts
@@ -11,6 +11,7 @@ export const env = createEnv({
     DATABASE_URL: z.url(),
     BETTER_AUTH_SECRET: z.string(),
     BETTER_AUTH_URL: z.string(),
+    RESEND_API_KEY: z.string(),
   },
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
@@ -21,6 +22,7 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
     BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
   },
   emptyStringAsUndefined: true,
 });
