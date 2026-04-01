3a0893ecc0253cd2bfa3a6295a9fb70203b48413
Victoriano <fvictoriano@hrplabs.com>
2026-04-01 16:14:50 -0400

    [feat] config for email on password reset and email verification

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,12 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { admin } from "better-auth/plugins";
 import { db } from "@/db/index";
+import { email } from "@f/mail";
 import * as authSchema from "@/db/schemas/auth-schema";
+import {
+  ResetPasswordTemplate,
+  SignUpTemplate,
+} from "@f/mail/components/templates";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -12,6 +17,24 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
     revokeSessionsOnPasswordReset: true,
+    sendResetPassword: async ({ user, url }) => {
+      email.send({
+        from: "Acme <onboarding@resend.dev>",
+        to: [user.email],
+        subject: "Email verification",
+        react: ResetPasswordTemplate({ name: user.name, url }),
+      });
+    },
+  },
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url }) => {
+      email.send({
+        from: "Acme <onboarding@resend.dev>",
+        to: [user.email],
+        subject: "Email verification",
+        react: SignUpTemplate({ name: user.name, url }),
+      });
+    },
   },
   plugins: [admin()],
 });
