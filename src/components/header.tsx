3af8dd8ad3b98e48cab59986caf519827effc399
Francisco Victoriano <fvictoriano.dev@gmail.com>
2026-04-02 14:52:31 -0400

    [refactor] changed auth components implementation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,16 @@
 "use client";
-// Framework
-import { usePathname } from "next/navigation";
 // Utils
 import { cn } from "@/lib/utils";
 //Components
 import Link from "next/link";
 import { Container } from "@c/container";
-import { Button } from "./ui/button";
+import { Button } from "@c/ui/button";
 import { AuthLoading, SignedIn, SignedOut } from "@c/auth/auth";
 
 export function Header({
   className,
   ...props
 }: React.ComponentProps<"header">) {
-  const path = usePathname();
   return (
     <header
       {...props}
@@ -27,30 +24,24 @@ export function Header({
           Project<span className="font-semibold">Template</span>
         </Link>
         <nav className="space-x-2">
-          <AuthLoading />
-          <SignedIn>
-            <Button asChild>
-              <Link
-                href="/dashboard"
-                className={cn(
-                  path === "/dashboard" && "text-blue-500",
-                )}
+          <AuthLoading>
+            <SignedIn>
+              <Button asChild>
+                <Link href="/dashboard">Dashboard</Link>
+              </Button>
+            </SignedIn>
+            <SignedOut>
+              <Button
+                variant="secondary"
+                asChild
               >
-                Dashboard
-              </Link>
-            </Button>
-          </SignedIn>
-          <SignedOut>
-            <Button
-              variant="secondary"
-              asChild
-            >
-              <Link href="/sign-in">Sign in</Link>
-            </Button>
-            <Button asChild>
-              <Link href="/sign-up">Sign up</Link>
-            </Button>
-          </SignedOut>
+                <Link href="/sign-in">Sign in</Link>
+              </Button>
+              <Button asChild>
+                <Link href="/sign-up">Sign up</Link>
+              </Button>
+            </SignedOut>
+          </AuthLoading>
         </nav>
       </Container>
     </header>
